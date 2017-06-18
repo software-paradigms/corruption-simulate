e521fa08bc282e5be768501a43f045125d5870e8
pwener <phwener@gmail.com>
2017-06-17 22:36:53 -0300

    Change SpaceWindow class, to update JFrame

--- src/main/java/br/unb/fga/software/multiagent/SpaceWindow.java
+++ src/main/java/br/unb/fga/software/multiagent/SpaceWindow.java
@@ -17,23 +17,38 @@ public class SpaceWindow extends JFrame {
 	public static final Color HONEST = Color.WHITE;
 	public static final Color ARRESTED = Color.YELLOW;
 	
+	private Vector<JPanel> panels;
+	
 	public SpaceWindow(Integer gridSize){
 		super("Corrupt map Simulation");
 		setSize(500,500);
 		setLayout(new GridLayout(gridSize, gridSize));
+		panels = new Vector<>();
+		mountPainel(gridSize);
 	}
 	
 	/**
-	 * Creates the grid with the colors referring to the status of the agents
+	 * Update the grid with the colors referring to the status of the agents
 	 * @param color Get one with the list of colors that each agent will take. The matrix must be linearized
 	 */
-	public void mountPainel(Vector<Color> color) {
-		for (int i = 0; i < color.size(); i++) {
+	public void updatePainel(Vector<Color> color) {
+		for (int i = 0; i < panels.size(); i++) {
+			panels.elementAt(i).setBackground(color.get(i));
+		}
+	}
+
+	
+	/**
+	 * Creates the grid with the colors referring to the status of the agents
+	 */
+	private void mountPainel(Integer gridSize) {
+		for (int i = 0; i < gridSize * gridSize; i++) {
 			JPanel p = new JPanel();
 			p.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 			p.setSize(new Dimension(10, 10));
-			p.setBackground(color.get(i));
-			add(p);
+			p.setBackground(Color.CYAN);
+			panels.add(p);
+			this.add(p);
 		}
 	}
 }
